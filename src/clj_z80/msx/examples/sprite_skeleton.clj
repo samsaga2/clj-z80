e0099eef08951b4af66216363ffdf2a717b0c40e
Víctor M <victor@canelatools.com>
2018-04-09 11:41:50 +0200

    MSX keys functions

--- src/clj_z80/msx/examples/sprite_skeleton.clj
+++ src/clj_z80/msx/examples/sprite_skeleton.clj
@@ -3,6 +3,7 @@
             [clj-z80.msx.bios :as bios]
             [clj-z80.msx.sysvars :as sysvars]
             [clj-z80.msx.util.sprites :refer [convert-sprite-16x16]]
+            [clj-z80.msx.keys :as keys]
             [clj-z80.msx.sprites :as spr]
             [clojure.java.shell :refer [sh]]
             clj-z80.msx.image))
@@ -10,7 +11,7 @@
 (defasmproc skeleton {:page 1}
   (apply db (convert-sprite-16x16 "resources/sprites/skeleton.png")))
 
-(defasmproc entry {:page 0 :include-always true}
+(defasmproc init {}
   ;; screen 2,2
   [:xor :a]
   [:ld [sysvars/FORCLR] :a]
@@ -29,14 +30,30 @@
   [:call spr/clear-attributes]
   (spr/ld-ix-attributes 0)
   (spr/write-ix-attributes :y 20 :x 20 :pattern 0 :color 15)
-  [:call spr/write-attributes]
+  [:jp spr/write-attributes])
 
-  ;; main loop
+(defasmproc move {}
+  (keys/ld-a-key-row "RIGHT")
+  (keys/bit-key-pressed "RIGHT") [:call :z :right]
+  (keys/bit-key-pressed "LEFT")  [:call :z :left]
+  (keys/bit-key-pressed "UP")    [:call :z :up]
+  (keys/bit-key-pressed "DOWN")  [:call :z :down]
+  [:ret]
+  (label :right [:inc [:ix spr/+attribute-x+]] [:ret])
+  (label :left  [:dec [:ix spr/+attribute-x+]] [:ret])
+  (label :up    [:dec [:ix spr/+attribute-y+]] [:ret])
+  (label :down  [:inc [:ix spr/+attribute-y+]] [:ret]))
+
+(defasmproc main-loop {}
   (label :loop
-         [:inc [:ix spr/+attribute-x+]]
+         [:call move]
          [:call spr/write-attributes]
          [:halt]
          [:jr :loop]))
+
+(defasmproc entry {:page 0 :include-always true}
+  [:call init]
+  [:jp main-loop])
 
 (build-asm-image-file "test2.rom" :msx-rom32k)
 (sh "openmsx" "-carta" "test2.rom")
